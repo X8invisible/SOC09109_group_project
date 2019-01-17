03e3050d25f076772bad38a99fe31f4a27179338
Andrei <ovidiu.andrey97@gmail.com>
2019-01-17 12:47:46 +0000

    added board manager and game manager,changed the map, textures load glitch atm

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public BoardManager boardScript;
+    public static GameManager instance = null;
+
+
+    private int level = 1;
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
