0f9cf16cb40d19b708fd9408d9b84083edd4579d
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-01-27 22:36:01 +0000

    works with abstract class, but the up and down buttons need repair

--- Assets/Scripts/Controllers/Car.cs
+++ Assets/Scripts/Controllers/Car.cs
@@ -5,18 +5,15 @@ public class Car : Vehicle
     bool AccForward, AccBackward, Left, Right;
 
     private Transform transform;
-
-    Car car;
-
+    
     public Car(Transform transform)
     {
-        car = new Car(transform);
-        //this.transform = transform;
-        car.MaxSpeed = 7.0f;
-        car.MaxSteer = 2.0f;
-        car.Brakes = 0.2f;
-        car.Acceleration = 0.0f;
-        car.Steer = 0.0f;
+        this.transform = transform; 
+        this.MaxSpeed = 7.0f;
+        this.MaxSteer = 2.0f;
+        this.Brakes = 0.2f;
+        this.Acceleration = 0.0f;
+        this.Steer = 0.0f;
     }
     
     public override void Accelerate(int Direction)
@@ -27,8 +24,8 @@ public class Car : Vehicle
         {
             AccForward = true;
 
-            if (car.Acceleration <= car.MaxSpeed)
-                car.Acceleration += 0.05f;
+            if (this.Acceleration <= this.MaxSpeed)
+                this.Acceleration += 0.05f;
 
             if (Left)
             {
@@ -46,25 +43,24 @@ public class Car : Vehicle
         {
             AccBackward = true;
 
-            if ((-1 * car.MaxSpeed) <= car.Acceleration) // MinSpeed is MaxSpeed * -1 so they are symmetrical
-                car.Acceleration -= 0.05f;   // keep decelerating if MinSpeed hasn't been reached
+            if ((-1 * this.MaxSpeed) <= this.Acceleration) // MinSpeed is MaxSpeed * -1 so they are symmetrical
+                this.Acceleration -= 0.05f;   // keep decelerating if MinSpeed hasn't been reached
 
             if (Left)
             {
-                transform.Rotate(Vector3.forward * Steer);
+                transform.Rotate(Vector3.back * Steer);
                 Left = false;
             }
             else if (Right)
             {
-                transform.Rotate(Vector3.back * Steer);
+                transform.Rotate(Vector3.forward * Steer);
                 Right = false;
             }
         }
 
-        if (car.Steer <= car.MaxSteer)
-            car.Steer += 0.01f;
-
-       // transform what?? how can I connect the car that the player uses with this movement?
+        if (this.Steer <= this.MaxSteer)
+            this.Steer += 0.01f;
+        
         transform.Translate(Vector2.up * Acceleration * Time.deltaTime);
 
         Debug.Log("----end of Accelerate in Car.cs----");
@@ -76,9 +72,9 @@ public class Car : Vehicle
 
         if (Direction == 1) // stop accelerating forwards
         {
-            if (car.Acceleration >= 0.0f)
+            if (this.Acceleration >= 0.0f)
             {
-                car.Acceleration -= car.Brakes;
+                this.Acceleration -= this.Brakes;
 
                 if (Left)
                 {
@@ -96,9 +92,9 @@ public class Car : Vehicle
         }
         else if (Direction == -1)  // stop accelerating backwards
         {
-            if (car.Acceleration <= 0.0f)
+            if (this.Acceleration <= 0.0f)
             {
-                car.Acceleration += car.Brakes;
+                this.Acceleration += this.Brakes;
 
                 if (Left)
                 {
@@ -114,8 +110,8 @@ public class Car : Vehicle
             else
                 AccBackward = false;
         }
-        if (car.Steer >= 0.0f)
-            car.Steer -= 0.01f;
+        if (this.Steer >= 0.0f)
+            this.Steer -= 0.01f;
 
         transform.Translate(Vector2.up * Acceleration * Time.deltaTime);
 
@@ -127,7 +123,7 @@ public class Car : Vehicle
         Debug.Log("----RotateLeft----");
 
         Left = true;
-    }
+    } 
 
     public override void RotateRight()
     {
@@ -156,9 +152,9 @@ public class Car : Vehicle
         Debug.Log("----start StopCarMotion----");
 
         if (AccForward)
-            StopAcc(1, car.Brakes); // while moving forwards
+            StopAcc(1, this.Brakes); // while moving forwards
         else if (AccBackward)
-            StopAcc(-1, car.Brakes); // while moving backwards
+            StopAcc(-1, this.Brakes); // while moving backwards
 
         Debug.Log("----end StopCarMotion----");
     }
@@ -168,7 +164,7 @@ public class Car : Vehicle
     {
         Debug.Log("----start collision----");
 
-        car.Acceleration = 0.0f;
+        this.Acceleration = 0.0f;
 
         Debug.Log("----end collision----");
     }
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -18,140 +18,59 @@ public class PlayerController : MonoBehaviour
     bool AccelFwd, AccelBwd;
     bool SteerLeft, SteerRight;
 
-
-    //Vehicle car;
+    Car car;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("------START-------");
-
-        //car = new Car(transform);
+        car = new Car(transform);
     }
 
     // is called x amount of times per frame, so physics won't be applied every frame and will be smoother
     private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w")) //Accelerate forwards
-            Accel(1);
+        {
+            car.Accelerate(1);
+            
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))  // rotate to the right
+                car.RotateRight();
+
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))   // rotate to the left
+                car.RotateLeft();
+        }
 
         else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s")) //Accelerate backwards
-            Accel(-1);
+        {
+            car.Accelerate(-1);
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))  // rotate to the right
+                car.RotateRight();
+
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))   // rotate to the left
+                car.RotateLeft();
+        }
 
         else if (Input.GetKey(KeyCode.Space))  // Breaks
         {
             if (AccelFwd)
-                StopAccel(1, Breaks);   // Breaks while in forward direction
+                car.StopAcc(1, Breaks);   // Breaks while in forward direction
 
             else if (AccelBwd)
-                StopAccel(-1, Breaks);   // Breaks while in backward direction
+                car.StopAcc(-1, Breaks);   // Breaks while in backward direction
         }
         else
         {
             if (AccelFwd)  //Applies breaks slowly if no key is pressed while in forward direction
-                StopAccel(1, 0.1f);
+                car.StopAcc(1, 0.1f);
 
             else if (AccelBwd)    //Applies breaks slowly if no key is pressed while in backward direction
-                StopAccel(-1, 0.1f);
+                car.StopAcc(-1, 0.1f);
         }
     }
 
-
-
-
-
-
-
-    // Acceleration of the car
-    public void Accel(int Direction)
-    {
-        Debug.Log("------start Accel-------");
-        if (Direction == 1) // forwards
-        {
-            AccelFwd = true;   // start acceleration
-
-            if (Acceleration <= MaxSpeed) // if acceleration hasn't reached the maximum speed, keep accelerating
-                Acceleration += 0.05f;
-
-            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))   // rotate to the left
-                transform.Rotate(Vector3.forward * Steer);
-
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))  // rotate to the right
-                transform.Rotate(Vector3.back * Steer);
-
-        }
-        else if (Direction == -1) // backwards
-        {
-            AccelBwd = true;  // start acceleration backwards
-
-            if ((-1 * MaxSpeed) <= Acceleration) // MinSpeed is MaxSpeed * -1 so they are symmetrical
-                Acceleration -= 0.05f;   // keep decelerating if MinSpeed hasn't been reached
-
-            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
-                transform.Rotate(Vector3.back * Steer);
-
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
-                transform.Rotate(Vector3.forward * Steer);
-
-        }
-
-        if (Steer <= MaxSteer)
-            Steer += 0.01f;
-
-        // Move the transform of the car in the direction and distance of translation.
-        transform.Translate(Vector2.up * Acceleration * Time.deltaTime);
-        // vector2.up = vector(0,1) = move up
-
-        Debug.Log("------end Accel-------");
-    }
-
-
-
-
-
-
-
-    // stop acceleration of car
-    public void StopAccel(int Direction, float BreakingFactor)
-    {
-        Debug.Log("------start stopAccel-------");
-
-        if (Direction == 1)  // stop accelerating forwards
-        {
-            if (Acceleration >= 0.0f)
-            {
-                Acceleration -= BreakingFactor;
-
-                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
-                    transform.Rotate(Vector3.forward * Steer);
-                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
-                    transform.Rotate(Vector3.back * Steer);
-            }
-            else
-                AccelFwd = false;
-        }
-        else if (Direction == -1)  // stop accelerating backwards
-        {
-            if (Acceleration <= 0.0f)
-            {
-                Acceleration += BreakingFactor;
-
-                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
-                    transform.Rotate(Vector3.back * Steer);
-                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
-                    transform.Rotate(Vector3.forward * Steer);
-            }
-            else
-                AccelBwd = false;
-        }
-        if (Steer >= 0.0f)
-            Steer -= 0.01f;
-
-        transform.Translate(Vector2.up * Acceleration * Time.deltaTime);
-
-        Debug.Log("------end stopAccel-------");
-    }
-
+    
     
 
 
