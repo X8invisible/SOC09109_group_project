4a692c852588f5b217395eae431e60ed2af7e73f
Andrei <ovidiu.andrey97@gmail.com>
2019-02-06 13:08:52 +0000

    changed controller mapping

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -31,31 +31,31 @@ public class PlayerController : MonoBehaviour
     // is called x amount of times per frame, so physics won't be applied every frame and will be smoother
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w") || Input.GetButton("CarForward")) //Accelerate forwards
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w") || (Input.GetAxis("CarForward")>0)) //Accelerate forwards
         {
             car.Accelerate(1);
 
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d") || Input.GetButton("CarRight"))
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d") || (Input.GetAxis("CarDirection")>0))
             {  // rotate to the right
                 car.RotateRight();
             }
-            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a") || Input.GetButton("CarLeft"))
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a") || (Input.GetAxis("CarDirection")<0))
             {
                 // rotate to the left
                 car.RotateLeft();
             }
         }
 
-        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s") || Input.GetButton("CarBack")) //Accelerate backwards
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s") || (Input.GetAxis("CarBack")>0)) //Accelerate backwards
         {
             car.Accelerate(-1);
 
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d") || Input.GetButton("CarRight"))
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d") || (Input.GetAxis("CarDirection")>0))
             {
                 // rotate to the right
                 car.RotateRight();
             }
-            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a") || Input.GetButton("CarLeft"))
+            else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a") || (Input.GetAxis("CarDirection")<0))
             { // rotate to the left
                 car.RotateLeft();
             }
