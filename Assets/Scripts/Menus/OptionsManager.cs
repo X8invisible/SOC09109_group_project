c4f56605523934a6de11bed6d70d52c753f291c5
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-04-08 13:55:53 +0100

    background change and HUD and night mode option

---
+++ Assets/Scripts/Menus/OptionsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour
+{
+    bool checkbox = true;
+    public GameObject lights;
+    public GameObject directional;
+
+    public void OnValueChanged()
+    {
+        Debug.Log(checkbox);
+        lights.SetActive(checkbox);
+        directional.SetActive(!checkbox);
+        checkbox = !checkbox;
+    }
+}
